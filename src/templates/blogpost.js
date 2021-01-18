59e02a3d12d71edb075c6dc4fcef5d7f39a2eb6f
OrangeBurrito <mali.kewl.pool@gmail.com>
2021-01-18 16:17:54 +0700

    blog post template shows mdx data

--- src/templates/blogpost.js
+++ src/templates/blogpost.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import {graphql} from 'gatsby'
+import {MDXRenderer} from 'gatsby-plugin-mdx'
 
 import Layout from '../components/layout'
 
-export default () => {
+export const query = graphql `
+query PostBySlug($slug: String!) {
+  mdx(fields: { slug: { eq: $slug } }) {
+		body
+    frontmatter {
+      title
+      date(formatString: "YYYY MMMM Do")
+    }
+  }
+}
+`
+
+export default ({data}) => {
+	const {frontmatter, body} = data.mdx
 	return (
 		<Layout>
-			<p>blogpost template</p>
+			<h1>{frontmatter.title}</h1>
+			<p style={{borderBottom: '2px solid #fff'}}>{frontmatter.date}</p>
+			<MDXRenderer>{body}</MDXRenderer>
 		</Layout>
 	)
 }
