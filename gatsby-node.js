fb11c3ea8ca039e9a0b078a4f625862b656b3a3f
OrangeBurrito <mali.kewl.pool@gmail.com>
2021-01-21 16:13:28 +0700

    fix gatsby-node stuff

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-const { createFilePath, createRemoteFileNode } = require(`gatsby-source-filesystem`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 const path = require(`path`)
 
 exports.createPages = ({ actions, graphql }) => {
@@ -27,9 +27,6 @@ exports.createPages = ({ actions, graphql }) => {
 		const posts = result.data.allMdx.nodes
 
 		posts.forEach((post, index) => {
-
-	// 		const previous = index === posts.length - 1 ? null : posts[index + 1];
-	// const next = index === 0 ? null : posts[index - 1];
 	
 			const	previous = index === 0 ? null : posts[index - 1]
 			const next = index === posts.length - 1 ? null : posts[index + 1]
@@ -47,21 +44,7 @@ exports.createPages = ({ actions, graphql }) => {
 	})
 }
 
-exports.createSchemaCustomization = ({ actions }) => {
-  const { createTypes } = actions
-  createTypes(`
-    type Mdx implements Node {
-      frontmatter: Frontmatter
-      coverImage: File @link(from: "coverImage___NODE")
-    }
-    type Frontmatter {
-      title: String!
-      coverImage: String
-    }
-  `)
-}
-
-exports.onCreateNode = async ({ node, actions, getNode, store, cache, createNodeId}) => {
+exports.onCreateNode = ({ node, actions, getNode}) => {
 	const { createNodeField, createNode } = actions;
 	if (node.internal.type === `Mdx`) {
 		const value = createFilePath({ node, getNode });
@@ -71,22 +54,4 @@ exports.onCreateNode = async ({ node, actions, getNode, store, cache, createNode
 			value,
 		});
 	}
-
-	if (
-    node.internal.type === "Mdx" &&
-    node.frontmatter.coverImage !== null
-  ) {
-    let fileNode = await createRemoteFileNode({
-      url: node.frontmatter.coverImage, // string that points to the URL of the image
-      parentNodeId: node.id, // id of the parent node of the fileNode you are going to create
-      createNode, // helper function in gatsby-node to generate the node
-      createNodeId, // helper function in gatsby-node to generate the node id
-      cache, // Gatsby's cache
-      store, // Gatsby's Redux store
-    })
-    // if the file was created, attach the new node to the parent node
-    if (fileNode) {
-      node.coverImage___NODE = fileNode.id
-    }
-  }
 }
